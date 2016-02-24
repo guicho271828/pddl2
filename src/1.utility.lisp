41f2ffd4bd0e47dae3a2dd7df5a4d99d79a8ca69
Masataro Asai <guicho2.71828@gmail.com>
2016-02-24 23:24:01 +0900

    import do-restart

--- src/1.utility.lisp
+++ src/1.utility.lisp
@@ -12,3 +12,32 @@
      (with-gensyms (tmp)
        `((lambda (,tmp) (,head ,tmp ,@args))
          ,(compile--> rest))))))
+
+(defmacro do-restart (bindings &body body)
+  "A construct that, after a restart is invoked, it jumps to the start and reevaluate
+the body. Example:
+
+ (do-restart ((retry (lambda (c) (print :retry)))
+           (continue (lambda (c) (print :retry))))
+   (error \"error!\"))
+"
+  (with-gensyms (start)
+    `(block nil
+       (tagbody
+          ,start
+          (return
+            (restart-bind
+                ,(mapcar
+                  (lambda (binding)
+                    (destructuring-bind
+                          (name function . key-value-pair)
+                        binding
+                      (with-gensyms (rest)
+                        `(,name (named-lambda ,(concatenate-symbols name 'handler)
+                                    (&rest ,rest)
+                                  (prog1
+                                      (apply ,function ,rest)
+                                    (go ,start)))
+                                ,@key-value-pair))))
+                  bindings)
+              ,@body))))))
